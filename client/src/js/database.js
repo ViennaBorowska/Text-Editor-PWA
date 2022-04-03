9af7cc9199526682fb79f7ee172ef7a0440706f3
Vienna Borowska <vienna.borowska@hotmail.com>
2022-04-03 12:50:38 +0100

    removed console logs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,41 +12,32 @@ const initdb = async () =>
     },
   });
 
-export const postDb = async (id, value) => {
-  if (id && value) {
-    console.log("Post to the database");
-
-    // Create a connection to the database database and version we want to use.
-    const jateDb = await openDB("jate", 1);
-    // Create a new transaction and specify the database and data privileges.
-    const userText = jateDb.transaction("jate", "readwrite");
-    // Open up the desired object store.
-    const objStore = userText.objectStore("jate");
-    // Use the .add() method on the store and pass in the content.
-    const request = objStore.add({ id: id, value: value });
-    // Get confirmation of the request.
-    const result = await request;
-    console.log("🚀 - data saved to the database", result);
-  }
-  console.error("Post to database not implemented :(");
+export const putDb = async (value) => {
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges.
+  const userText = jateDb.transaction("jate", "readwrite");
+  // Open up the desired object store.
+  const objStore = userText.objectStore("jate");
+  // Use the .add() method on the store and pass in the content.
+  const request = objStore.put({ value });
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
 };
 
 export const getDb = async () => {
-  try {
-    // Create a connection to the database database and version we want to use.
-    const jateDb = await openDB("jate", 1);
-    // Create a new transaction and specify the database and data privileges.
-    const userText = jateDb.transaction("jate", "readonly");
-    // Open up the desired object store.
-    const objStore = userText.objectStore("jate");
-    // Use the .getAll() method to get all data in the database.
-    const request = objStore.getAll();
-    // Get confirmation of the request.
-    const result = await request;
-    console.log("Found results from database", result);
-  } catch {
-    console.error("getDb not implemented");
-  }
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB("jate", 1);
+  // Create a new transaction and specify the database and data privileges.
+  const userText = jateDb.transaction("jate", "readonly");
+  // Open up the desired object store.
+  const objStore = userText.objectStore("jate");
+  // Use the .getAll() method to get all data in the database.
+  const request = objStore.getAll();
+  // Get confirmation of the request.
+  const result = await request;
+  console.log("Found results from database", result);
 };
 
 initdb();
